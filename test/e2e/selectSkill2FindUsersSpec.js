6a2d2293ea7902e03e22b8597df1a23cdca67274
JoeWroe <joe_wroe@icloud.com>
2016-03-23 15:00:35 +0000

    e2e TEST
    Added tests to 'selectSkill2FindUsersSpec.js'.
    Followed user flow to selecting a skill.
    Added a few id's to 'index.html':
    	skillSelectorButtons
    	listUsersWithSelectedSkills

--- test/e2e/selectSkill2FindUsersSpec.js
+++ test/e2e/selectSkill2FindUsersSpec.js
@@ -7,8 +7,22 @@ describe('SOS Fixit search skills feature', function() {
   describe('on page load', function() {
 
     it('allows skills to be selected and provides a button to search them', function() {
+      expect(element(by.id('skillSelectorButtons')).isPresent()).toBe(true);
       expect(element(by.id('skillSearchSubmit')).isDisplayed()).toBe(true);
       expect(element(by.id('skillSearchSubmit')).getText()).toContain('Search');
+      expect(element(by.id('listUsersWithSelectedSkills')).isPresent()).toBe(true);
+      expect(element(by.id('listUsersWithSelectedSkills')).isDisplayed()).toBe(false);
+    });
+
+    describe('on submiting an empty search', function() {
+
+      
+
+      it('doesn\'t display a list of users', function() {
+        expect(element(by.id('listUsersWithSelectedSkills')).isPresent()).toBe(true);
+        expect(element(by.id('listUsersWithSelectedSkills')).isDisplayed()).toBe(false);
+      });
+
     });
   });
 });
