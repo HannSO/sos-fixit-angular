6a68f1c71dd180df8bd046e17b60d3ab25da909e
JoeWroe <joe_wroe@icloud.com>
2016-03-23 10:51:48 +0000

    e2e TEST
    Added protractor tests for search button.

--- test/e2e/selectSkill2FindUsersSpec.js
+++ test/e2e/selectSkill2FindUsersSpec.js
@@ -1,10 +1,14 @@
-describe('SOS Fixit Search', function() {
+describe('SOS Fixit search skills feature', function() {
 
   beforeEach(function() {
     browser.get('http://localhost:8080');
   });
 
-  it('allows you to search by skill', function() {
-    element(by.id('skillSearchSubmit')).click();
+  describe('on page load', function() {
+
+    it('allows skills to be selected and provides a button to search them', function() {
+      expect(element(by.id('skillSearchSubmit')).isDisplayed()).toBe(true);
+      expect(element(by.id('skillSearchSubmit')).getText()).toContain('Search');
+    });
   });
 });
