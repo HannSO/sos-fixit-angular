43b31b3461ec24c6f35d9561519529647b4df25d
ZeshanRasul <zeshanrasul90@gmail.com>
2016-03-25 18:48:00 +0000

    Refactored user_sessions controller to use self instead of scope

--- public/js/controllers/user_sessions.js
+++ public/js/controllers/user_sessions.js
@@ -1,8 +1,11 @@
 angular.module('SosFixit')
-  .controller('UserSessionsCtrl', ['$scope', function($scope){
-    $scope.$on('auth:login-error', function(ev, reason){
-      $scope.error= reason.errors[0];
+  .controller('UserSessionsCtrl', function(){
+
+    var self = this;
+
+    self.$on('auth:login-error', function(ev, reason){
+      self.error= reason.errors[0];
     });
-    $scope.login = true;
+    self.login = true;
 
   }]);
