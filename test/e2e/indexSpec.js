1d3bc433e92425885f47db57d14b5cb927e75863
JoeWroe <joe_wroe@icloud.com>
2016-03-25 14:44:55 +0000

    PROTRACTOR TESTS FOR HOMEPAGE
    Added testing for all features of 'index.html' MVP.
    All tests currently passing

--- test/e2e/indexSpec.js
+++ test/e2e/indexSpec.js
@@ -1,4 +1,4 @@
-describe('SOS Fixit', function() {
+describe('SOS Fixit homepage', function() {
 
   beforeEach(function() {
     browser.get('/#');
@@ -8,6 +8,34 @@ describe('SOS Fixit', function() {
 
     it('Should display the correct title', function() {
       expect(browser.getTitle()).toBe('SOS Fixit');
+    });
+
+    it('Should provide options to sign in and register', function() {
+      expect(element(by.id('signInLink')).isDisplayed()).toBe(true);
+      element(by.id('signInLink')).getText().then(function(text) {
+        console.log('found text "' + text + '" at id="signInLink"');
+      });
+      expect(element(by.id('registerLink')).isDisplayed()).toBe(true);
+      element(by.id('registerLink')).getText().then(function(text) {
+        console.log('found text "' + text + '" at id="registerLink"');
+      });
+    });
+
+    it('Should not think any users are signed in', function() {
+      expect(element(by.id('welcomeUser')).isDisplayed()).toBe(false);
+    });
+  });
+
+  describe('Selection of links on page', function() {
+
+    it('Takes you to the sign in page', function() {
+      element(by.linkText('Sign in')).click();
+      expect(browser.getCurrentUrl()).toBe('http://localhost:8080/#/sign_in');
+    });
+
+    it('Takes you to the Register page', function() {
+      element(by.linkText('Register')).click();
+      expect(browser.getCurrentUrl()).toBe('http://localhost:8080/#/register');
     });
   });
 
