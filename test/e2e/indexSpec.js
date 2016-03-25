c4ba7eba65f118d5587147b52457aca9aa5c234a
JoeWroe <joe_wroe@icloud.com>
2016-03-25 13:43:56 +0000

    PROTRACTOR TESTING
    Added 'jasmine-spec-reporter'.
    Updated 'protractor.conf.js' toreflect this.
    Added 'beforeEach get homepage' to 'indexSpec.js'.

--- test/e2e/indexSpec.js
+++ test/e2e/indexSpec.js
@@ -1,9 +1,12 @@
 describe('SOS Fixit', function() {
 
+  beforeEach(function() {
+    browser.get('/#');
+  });
+
   describe('On page load of "index.html"', function() {
 
     it('Should display the correct title', function() {
-      browser.get('/#');
       expect(browser.getTitle()).toBe('SOS Fixit');
     });
   });
--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,9 +1,22 @@
 exports.config = {
+
   framework: 'jasmine',
 
   specs: [
     'e2e/**/*Spec.js'
   ],
 
-  baseUrl: 'http://localhost:8080'
+  baseUrl: 'http://localhost:8080',
+
+  onPrepare: function() {
+      var SpecReporter = require('jasmine-spec-reporter');
+      // add jasmine spec reporter
+      jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
+   },
+
+   jasmineNodeOpts: {
+     print: function() {},
+     showColors: true,
+     defaultTimeoutInterval: 30000
+   }
 };
