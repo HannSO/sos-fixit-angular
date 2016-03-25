0dadf79c50eac1dda85a54c571717a19fde36047
JoeWroe <joe_wroe@icloud.com>
2016-03-25 11:56:15 +0000

    PROTRACTOR TESTING
    Commented out all previously written tests, starting fresh from walkthrough at:
    	https://egghead.io/lessons/angularjs-getting-started-with-protractor

--- test/e2e/selectSkill2FindUsersSpec.js
+++ test/e2e/selectSkill2FindUsersSpec.js
@@ -1,28 +1,30 @@
 describe('SOS Fixit search skills feature', function() {
 
-  beforeEach(function() {
-    browser.get('http://localhost:8080');
-  });
-
-  describe('on page load', function() {
-
-    it('allows skills to be selected and provides a button to search them', function() {
-      expect(element(by.id('skillSelectorButtons')).isPresent()).toBe(true);
-      expect(element(by.id('skillSearchSubmit')).isDisplayed()).toBe(true);
-      expect(element(by.id('skillSearchSubmit')).getText()).toContain('Search');
-      expect(element(by.id('listUsersWithSelectedSkills')).isPresent()).toBe(true);
-      expect(element(by.id('listUsersWithSelectedSkills')).isDisplayed()).toBe(false);
-    });
-
-    describe('on submiting an empty search', function() {
 
 
-
-      it('doesn\'t display a list of users', function() {
-        expect(element(by.id('listUsersWithSelectedSkills')).isPresent()).toBe(true);
-        expect(element(by.id('listUsersWithSelectedSkills')).isDisplayed()).toBe(false);
-      });
-
-    });
-  });
+  // beforeEach(function() {
+  //   browser.get('http://localhost:8080');
+  // });
+  //
+  // describe('on page load', function() {
+  //
+  //   it('allows skills to be selected and provides a button to search them', function() {
+  //     expect(element(by.id('skillSelectorButtons')).isPresent()).toBe(true);
+  //     expect(element(by.id('skillSearchSubmit')).isDisplayed()).toBe(true);
+  //     expect(element(by.id('skillSearchSubmit')).getText()).toContain('Search');
+  //     expect(element(by.id('listUsersWithSelectedSkills')).isPresent()).toBe(true);
+  //     expect(element(by.id('listUsersWithSelectedSkills')).isDisplayed()).toBe(false);
+  //   });
+  //
+  //   describe('on submiting an empty search', function() {
+  //
+  //
+  //
+  //     it('doesn\'t display a list of users', function() {
+  //       expect(element(by.id('listUsersWithSelectedSkills')).isPresent()).toBe(true);
+  //       expect(element(by.id('listUsersWithSelectedSkills')).isDisplayed()).toBe(false);
+  //     });
+  //
+  //   });
+  // });
 });
