be0bdc26e99abf1a6bf22d7c7a29e37a18dd6df6
JoeWroe <joe_wroe@icloud.com>
2016-03-27 16:21:35 +0100

    PROTRACTOR userRegisterHelper
    Finished writting helper.
    Testing helper and working once.
    Helper failing if user is already registed.

--- test/e2e/helpers/registerUserHelper.js
+++ test/e2e/helpers/registerUserHelper.js
@@ -1,5 +1,19 @@
-var registerUser = function() {
+var testEmail;
+var testPassword;
+
+var registerUserHelper = function() {
+
+  testEmail    = 'test@test.com';
+  testPassword = 'testpassword';
+
+  this.registerUser = function() {
+    browser.get('/#/register');
+    element(by.css('[type="email"]')).sendKeys(testEmail);
+    element(by.model('registrationForm.password')).sendKeys(testPassword);
+    element(by.model('registrationForm.password_confirmation')).sendKeys(testPassword);
+    element(by.css('[type="submit"]')).click();
+  };
 
 };
 
-module.exports = new registerUser();
+module.exports = new registerUserHelper();
--- test/e2e/signInSpec.js
+++ test/e2e/signInSpec.js
@@ -2,6 +2,7 @@ describe('SOS Fixit Sign In partial', function() {
 
   var email;
   var password;
+  var registerUserHelper = require('./helpers/registerUserHelper.js');
 
   beforeEach(function() {
     email = 'joe_wroe@icloud.com';
@@ -19,7 +20,22 @@ describe('SOS Fixit Sign In partial', function() {
     });
 
     it('Should provide a button to sign in', function() {
+      expect(element(by.css('[type="submit"]')).isDisplayed()).toBe(true);
+    });
+  });
 
+  describe('Logging in', function() {
+
+    beforeEach(function() {
+      registerUserHelper.registerUser();
+    });
+
+    // afterEach(function() {
+    //   bin/rake db:reset;
+    // });
+
+    it('Should have created a new user and signed them in', function() {
+      expect(browser.getCurrentUrl()).toBe('http://localhost:8080/#/');
     });
   });
 });
