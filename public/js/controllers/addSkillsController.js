8601864fb8bceca17738f0557e8bbc4bf40c9203
ZeshanRasul <zeshanrasul90@gmail.com>
2016-03-29 00:15:44 +0100

    User can add pre-existing skills to their themselves

--- public/js/controllers/addSkillsController.js
+++ public/js/controllers/addSkillsController.js
@@ -1,12 +1,16 @@
 angular.module('SosFixit')
-.controller('addSkillsController', ['skillsResourceFactory', function(skillsResourceFactory) {
+.controller('addSkillsController', ['skillsResourceFactory', 'skillsListService', '$rootScope', '$http', function(skillsResourceFactory, skillsListService, $rootScope, $http) {
 var self = this;
 
 self.skill = '';
+self.skills = [];
+self.users = [];
+self.searchParam = '';
 
   (self.getSkillsList = function() {
     skillsResourceFactory.getSkillsList()
       .then(function(json) {//
+        console.log("get skills list");
       for (var i = 0; i < json.data.length; i ++){
         self.skills.push(json.data[i].skill);
       }
@@ -14,10 +18,18 @@ self.skill = '';
   });
 
 
-  self.submit = function(){
-    var skill = self.skill ;
-    var currentUser = $rootScope.user.id ;
-    var userEditPath = "http://localhost:3000/users/edit/";
-    $http.post(userEditPath,{user: currentUser, skill: skill});
+  self.saveSkills = function(skill){
+    self.searchParam = skill.id;
+    skillsListService.setData(self.searchParam);
+    self.users = [];
+    console.log(self.searchParam);
   };
+
+  self.submit = function(){
+    var currentUser = $rootScope.user.id ;
+    var userEditPath = "http://localhost:3000/users/" + currentUser;
+    $http.patch(userEditPath,{user: currentUser, skill: self.searchParam});
+    console.log("post");
+  };
+
 }]);
