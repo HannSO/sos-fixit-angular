9376f8edd2e2e4c909e5b4fe0ecc12ef9d89d27a
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-03-29 12:11:47 +0100

    added checkbox functionality to add skills

--- public/js/controllers/addSkillsController.js
+++ public/js/controllers/addSkillsController.js
@@ -1,8 +1,9 @@
 angular.module('SosFixit')
-.controller('addSkillsController', ['skillsResourceFactory', function(skillsResourceFactory) {
+.controller('addSkillsController', ['skillsResourceFactory', '$rootScope', '$http', function(skillsResourceFactory, $rootScope, $http) {
 var self = this;
 
-self.skill = '';
+self.addedSkills = [];
+self.skills =[];
 
   (self.getSkillsList = function() {
     skillsResourceFactory.getSkillsList()
@@ -10,14 +11,25 @@ self.skill = '';
       for (var i = 0; i < json.data.length; i ++){
         self.skills.push(json.data[i].skill);
       }
+
     });
   });
 
+  self.addSkills = function(skill){
+    if (!skill.added){
+      var index = self.addedSkills.indexOf(skill.id);
+      self.addedSkills.splice(index, 1);
+    } else {
+      self.addedSkills.push(skill.id);
+    }
+  };
 
   self.submit = function(){
-    var skill = self.skill ;
+    console.log(self.addedSkills);
+    var skill = "potato" ;
     var currentUser = $rootScope.user.id ;
-    var userEditPath = "http://localhost:3000/users/edit/";
-    $http.post(userEditPath,{user: currentUser, skill: skill});
+    var userEditPath = "http://localhost:3000/users/" + currentUser;
+    var path = $http.patch(userEditPath,{user: currentUser, skills: skill});
+    console.log(path);
   };
 }]);
