0b4e9b97b4475b60247e0b3c61558bd7bb6aee0a
JoeWroe <joe_wroe@icloud.com>
2016-03-30 12:26:08 +0100

    PROTRACTOR REFACTOR
    Removed the database cleaner... Not sure why it was still in there.
    Refactored 'signInSpec.js' to DRY it up.

--- test/e2e/signInSpec.js
+++ test/e2e/signInSpec.js
@@ -1,27 +1,26 @@
-var DatabaseCleaner = require('database-cleaner');
-var databaseCleaner = new DatabaseCleaner('postgresql');
-
-
-
 describe('SOS Fixit Sign In partial', function() {
 
   var registerUserHelper    = require('./helpers/registerUserHelper.js');
   var signInUserHelper      = require('./helpers/signInUserHelper.js');
   var synchronizationHelper = require('./helpers/synchronizationHelper.js');
 
+  var emailField    = element(by.css('[type="email"]'));
+  var passwordField = element(by.css('[type="password"]'));
+  var submitButton  = element(by.css('[type="submit"]'));
+
   beforeEach(function() {
     browser.get('/#/sign_in');
   });
 
-  xdescribe('On page load of "user_sessions/new.html"', function() {
+  describe('On page load of "user_sessions/new.html"', function() {
 
     it('Should provide fields to enter an e-mail and password', function() {
-      expect(element(by.css('[type="email"]')).isDisplayed()).toBe(true);
-      expect(element(by.css('[type="password"]')).isDisplayed()).toBe(true);
+      expect(emailField.isDisplayed()).toBe(true);
+      expect(passwordField.isDisplayed()).toBe(true);
     });
 
     it('Should provide a button to sign in', function() {
-      expect(element(by.css('[type="submit"]')).isDisplayed()).toBe(true);
+      expect(submitButton.isDisplayed()).toBe(true);
     });
   });
 
