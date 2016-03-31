045c218fbd4094c295014dde6c53ec87f2ba3e79
ZeshanRasul <zeshanrasul90@gmail.com>
2016-03-31 22:30:01 +0100

    Job object created on 'request job' click with correct recipient and fixer ID

--- public/js/controllers/sosFixitMailboxController.js
+++ public/js/controllers/sosFixitMailboxController.js
@@ -67,25 +67,29 @@ sosFixit.controller('mailboxController', ['mailboxFactory', 'mailboxService','re
   self.createJob = function() {
     var currentUser = $rootScope.user.id;
     mailboxService.getData(self.conversationId);
-
     mailboxFactory.getMessages(self.conversationId)
     .then(function(json){
       for (var i = 0; i < json.data.conversation.mailboxer_receipts.length; i++){
         var inboxMessages = (json.data.conversation.mailboxer_receipts[i].mailboxer_receipt.mailbox_type == 'inbox');
         if (json.data.conversation.mailboxer_receipts[i].mailboxer_receipt.mailbox_type == 'inbox'){
-          self.recipientId = json.data.conversation.mailboxer_receipts[1].mailboxer_receipt.message.user.id;
-          if (self.recipientId !== $rootScope.user.id){
-            self.senderId = self.recipientId;
-            console.log(self.senderId);
-          } else {
-            self.fixerId = self.recipientId;
-            console.log(self.recipientId);
-          }
+          console.log(json);
+          self.fixerId = json.data.conversation.mailboxer_receipts[0].mailboxer_receipt.message.user.id;
+          // if (self.recipientId !== $rootScope.user.id){
+          //   self.senderId = self.recipientId;
+          //   console.log(self.senderId);
+          // } else {
+          //   self.fixerId = self.recipientId;
+          //   console.log(self.recipientId);
+          // }
+
         }
       }
 
       var jobCreatePath = 'http://localhost:3000/jobs';
-      $http.post(jobCreatePath, {recipient_id: currentUser, fixerId: self.fixerId});
+      console.log("currentUser: " + currentUser);
+      console.log("Fixer: " + self.fixerId);
+
+      $http.post(jobCreatePath, {recipient_id: currentUser, fixer_id: self.fixerId});
     });
   };
 
